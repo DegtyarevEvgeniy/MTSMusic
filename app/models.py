0e5d019fc971e30d39b3b500b47d4df4d1baecda
Degtyarev Evgeniy <degtyarevea12@gmail.com>
2023-11-24 23:15:46 +0300

    start 2

--- app/models.py
+++ app/models.py
@@ -7,3 +7,12 @@ from jsonfield import JSONField
 
 
 
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    creator = models.ForeignKey(AccountConfig, on_delete=models.CASCADE)
+    amount_of_users = models.IntegerField()
+    song = models.CharField(max_length=255)
+    timer = models.IntegerField()
+
+    def __str__(self):
+        return self.name
